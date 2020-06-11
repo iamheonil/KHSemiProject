87fc59c03a70278febf0875a20a5fc57baf44a6e
Owner <Owner@DESKTOP-GG842JR>
2020-06-12 01:10:23 +0900

    document

---
+++ ActiveWare/src/web/service/impl/DocumentServiceImpl.java
@@ -0,0 +1,78 @@
+package web.service.impl;
+
+import java.sql.Connection;
+import java.util.List;
+
+import web.dto.Document;
+import web.service.face.DocumentService;
+
+public class DocumentServiceImpl implements DocumentService{
+
+	@Override
+	public List<Document> selectDocTmp() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Document> selectDocWaitAppro() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Document> selectDocProg() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Document> selectDocDraft() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Document> selectDocAppro() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Document> selectDocAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Document selectDocByDocnum() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void insertDoc(Connection conn, Document doc) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateDoc(Connection conn, Document doc) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteDoc(Connection conn, Document doc) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Document selectDocument(Document doc) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+}
