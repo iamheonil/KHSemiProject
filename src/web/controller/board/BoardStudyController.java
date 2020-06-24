bde72c5a5201cb78db9ecde78420da0c3d689773
kmc1210 <57215151+kmc1210@users.noreply.github.com>
2020-06-24 20:33:06 +0900

    김민철: 스터디모집 게시판 추가에 따른 해당 controller 추가

---
+++ src/web/controller/board/BoardStudyController.java
@@ -0,0 +1,50 @@
+package web.controller.board;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import web.dto.Board;
+import web.service.face.BoardService;
+import web.service.impl.BoardServiceImpl;
+import web.util.B_Paging;
+
+/**
+ * Servlet implementation class BoardStudyController
+ */
+@WebServlet("/board/study")
+public class BoardStudyController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	//Service 객체
+	private BoardService boardService = new BoardServiceImpl();
+	
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("게시판 목록 호출 완료 [GET]");
+		
+		// 요청 파라미터를 전달을 통해 Paging 객체 생성
+		B_Paging paging = boardService.getPaging(req);
+		System.out.println("BoardListController " + paging);
+		//Paging 결과 MODEL값 전달
+		req.setAttribute("paging", paging);
+		
+		// 공지사항 게시글 조회
+		List<Board> S_list = boardService.sList();
+		List<Board> N_list = boardService.nList();
+		//조회된 결과 view 전달
+		
+		req.setAttribute("S_list", S_list);
+		req.setAttribute("N_list", N_list);
+		System.out.println(S_list);
+		//View 지정
+		req.getRequestDispatcher("/WEB-INF/views/board/boardStudy.jsp").forward(req, resp);
+		
+	}
+}
