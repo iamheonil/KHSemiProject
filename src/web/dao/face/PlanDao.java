abac363801df0cab28cc57f05f226c5a5b7bbfce
Kim heonil <iamheonil@gmail.com>
2020-06-26 21:24:15 +0900

    김헌일 : 일정관리 계속 수정 중

--- src/web/dao/face/PlanDao.java
+++ src/web/dao/face/PlanDao.java
@@ -2,10 +2,26 @@ package web.dao.face;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import web.dto.Plan;
+import web.dto.User_basic;
 
 public interface PlanDao {
 	
-	public List<Plan> dbPlan();
+	/**
+	 * DB 값 조회를 위해 아이디 정보를 가져옵니다.
+	 * @param req - 가져 올 Request 정보
+	 * @return - Request 의 ID
+	 */
+
+	public User_basic dbGetId(HttpServletRequest req);
+	
+	/**
+	 * DB에 들어가 있는 Plan을 가져옵니다.
+	 * 
+	 * @return
+	 */
+	public List<Plan> dbPlan(HttpServletRequest req);
 
 }
--- src/web/service/face/PlanService.java
+++ src/web/service/face/PlanService.java
@@ -2,10 +2,20 @@ package web.service.face;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import web.dto.Plan;
 
 public interface PlanService {
-
-	public List<Plan> getPlan();
+	
+	/**
+	 * DB에 들어가 있는 Plan들을 조회합니다.
+	 * 
+	 * @return
+	 */
+	public List<Plan> getPlan(HttpServletRequest req);
+	
+	
+	
 	
 }
