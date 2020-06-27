a6c616d22b6b18d69fb187e978189fad752ae7d0
JangSunghyun <rkddkwlcptm@gmail.com>
2020-06-27 20:53:18 +0900

    장성현 : user_basic 테이블 변경으로 인한 수정

--- src/web/dto/User_basic.java
+++ src/web/dto/User_basic.java
@@ -1,6 +1,7 @@
 package web.dto;
 
 public class User_basic {
+	private int basicnum; // 사원번호
 	private int userid;		 // 사번
 	private String username; // 이름
 	private String userrank; //직급
@@ -8,38 +9,37 @@ public class User_basic {
 	
 	@Override
 	public String toString() {
-		return "User_basic [userid=" + userid + ", username=" + username + ", userrank=" + userrank + ", dept=" + dept
-				+ "]";
+		return "User_basic [basicnum=" + basicnum + ", userid=" + userid + ", username=" + username + ", userrank="
+				+ userrank + ", dept=" + dept + "]";
 	}
-
+	
+	public int getBasicnum() {
+		return basicnum;
+	}
+	public void setBasicnum(int basicnum) {
+		this.basicnum = basicnum;
+	}
 	public int getUserid() {
 		return userid;
 	}
-
 	public void setUserid(int userid) {
 		this.userid = userid;
 	}
-
 	public String getUsername() {
 		return username;
 	}
-
 	public void setUsername(String username) {
 		this.username = username;
 	}
-
 	public String getUserrank() {
 		return userrank;
 	}
-
 	public void setUserrank(String userrank) {
 		this.userrank = userrank;
 	}
-
 	public String getDept() {
 		return dept;
 	}
-
 	public void setDept(String dept) {
 		this.dept = dept;
 	}
