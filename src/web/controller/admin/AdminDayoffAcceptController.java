5d94f462ddc3f5f700a960622d7de2e7f5bf8e6c
Purepre <65704899+Purepre@users.noreply.github.com>
2020-06-30 20:39:41 +0900

    신지우: 휴가 리스트 기능 완료

---
+++ src/web/controller/admin/AdminDayoffAcceptController.java
@@ -0,0 +1,35 @@
+package web.controller.admin;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import web.service.face.DayoffService;
+import web.service.impl.DayoffServiceImpl;
+
+@WebServlet("/admin/dayoff/accept")
+public class AdminDayoffAcceptController extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	private DayoffService dayoffService = new DayoffServiceImpl();
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		String names = req.getParameter("names");
+
+		if (!"".equals(names) && names != null) {
+			dayoffService.accept(names);
+		}
+
+		// 메인으로 리다이렉트
+		resp.sendRedirect("/admin/view");
+
+	}
+
+}
